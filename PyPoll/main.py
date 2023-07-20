5a3c5e7f5fe2c7034800e97fc2ae433fdcf17a7c
acalametti <alexandracalametti@Alexandras-Air.usinternet.com>
2023-07-19 22:27:45 -0500

    Final code

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,46 +2,61 @@
 import os
 import csv 
 
+
+#read csv and create analysis txt file
+pypoll_report = open('analysis/Report.txt','w')
 poll_data = csv.reader(open('Resources/election_data.csv'))
 
-next(poll_data)
+#store header 
+pypoll_header = next(poll_data)
+
 
 #create variables 
 total_votes = 0 
-candidate_vote = 0 
+candidate_vote_1 = 0 
+candidate_vote_2 = 0
+candidate_vote_3 = 0
 
+#separate candidate vote variables - Saad 
 #create loop to go through our rows 
 for row in poll_data:
     
     #CALCULATE THE TOTAL NUMBER OF VOTES
     total_votes = total_votes +1
     
-    candidate = [2]
+    candidate = row[2]
     
-    #Loop through candidate names 
-    if candidate == ["Charles Casper Stockham"]:
-       candidate_vote = candidate_vote + 1
+    #Loop through candidate names with if statement
+    if candidate == "Charles Casper Stockham":
+       candidate_vote_1 = candidate_vote_1 + 1
+            
+    elif candidate == "Diana DeGette":
+        candidate_vote_2 = candidate_vote_2 +1
 
-    elif candidate == ["Diana DeGette"]:
-        candidate_vote = candidate_vote +1
+    elif candidate == "Raymon Anthony Doane":
+        candidate_vote_3 = candidate_vote_3 +1
+    
+    #calculate the percentage for each candidate
+    candidate_1_percent = (candidate_vote_1/total_votes)*100
+    candidate_2_percent = (candidate_vote_2/total_votes)*100
+    candidate_3_percent = (candidate_vote_3/total_votes)*100
 
-    elif candidate == ["Raymon Anthony Doane"]:
-        candidate_vote = candidate_vote +1
 
 
-
-#print the output table
+#print the output table by filling in with variables
 poll_output = f'''
 
 Election Results
 -------------------------
 Total Votes: {total_votes}
 -------------------------
-Charles Casper Stockham: 23.049% ({candidate_vote})
-Diana DeGette: 73.812% (272892)
-Raymon Anthony Doane: 3.139% (11606)
+Charles Casper Stockham: {candidate_1_percent:,.3f}% ({candidate_vote_1})
+Diana DeGette: {candidate_2_percent:,.3f}% ({candidate_vote_2})
+Raymon Anthony Doane: {candidate_3_percent:,.3f}% ({candidate_vote_3})
 -------------------------
 Winner: Diana DeGette
 '''
 
+#print results 
 print(poll_output)
+pypoll_report.write(poll_output)
