504784027c31ea17de90823e05da3720a045c385
acalametti <alexandracalametti@Alexandras-Air.usinternet.com>
2023-07-19 21:50:07 -0500

    Final code and notes for the project

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,52 +1,65 @@
+
+#read import necessary programs 
+import os
 import csv
-my_report = open('analysis/Report.txt','w')
-data = csv.reader(open('Resources/budget_data.csv'))
 
-next(data)
+#read in csv and creat txt file for analysis report
+pybank_report = open('analysis/Report.txt','w')
+pybank_data = csv.reader(open('Resources/budget_data.csv'))
 
+#store header 
+pybank_header = next(pybank_data)
+
+#assign variables 
 total = 0 
 months = 0
 total_change = 0
-pre_rev = 0
-inc = ["",0]
-dec = ["",0]
+previous_rev = 0
+increase = ["",0]
+decrease = ["",0]
 
-for row in data:
-    # months = months + 1
+for row in pybank_data:
+    # months = months + 1 (+= shortcut provided by Geronimo Perez)
     months += 1
     
     #make string into integer
-    rev = int(row[1])
+    revenue = int(row[1])
     
-    #the total 
-    total += rev
+    #the calculate the total revenue
+    total = total + revenue
 
-    change = rev - pre_rev
-    if pre_rev == 0:
+    #find the total change in revenue 
+    change = revenue - previous_rev
+    if previous_rev == 0:
         change = 0
-    
-    total_change += change
 
-    if change > inc[1]:
-        inc[0] = row[0]
-        inc[1] = change
-
-    if change < dec[1]:
-        dec[0] = row[0]
-        dec[1] = change
+   #add the change to the total change  
+    total_change = total_change + change
 
 
-    pre_rev = rev
+    #calculte the net change in profit/loss using if statment
+    if change > increase[1]:
+        increase[0] = row[0]
+        increase[1] = change
 
+    if change < decrease[1]:
+        decrease[0] = row[0]
+        decrease[1] = change
+
+    #make it so the the pevious revenue calculation becomes new starting point
+    previous_rev = revenue
+
+
+#fill in output table with variables created (Mapping :,2f shortcut provided by Geronimo Perez)
 output = f'''
 Financial Analysis
 ----------------------------
 Total Months: {months}
 Total: ${total:,}
 Average Change: ${total_change/(months-1):,.2f}
-Greatest Increase in Profits: {inc[0]} (${inc[1]:,})
-Greatest Decrease in Profits: {dec[0]} (${dec[1]:,})
+Greatest Increase in Profits: {increase[0]} (${increase[1]:,})
+Greatest Decrease in Profits: {decrease[0]} (${decrease[1]:,})
 '''
-
+#print outputs 
 print(output)
-my_report.write(output)
+pybank_report.write(output)
